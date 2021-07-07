d94730fe1525797ec5de185fa4ce54cce98aa391
Shanthosh Ravi <shanthoshravi2006@gmail.com>
2021-07-07 11:02:17 +0530

    Fix GPS

--- device-common.mk
+++ device-common.mk
@@ -113,7 +113,11 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-	android.hardware.gnss@1.0-impl.universal7420
+    android.hardware.gnss@1.0-impl \
+	android.hardware.gnss@1.0-service
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/gps/gps.conf:system/etc/gps.conf
 
 # Graphics
 PRODUCT_PACKAGES += \
